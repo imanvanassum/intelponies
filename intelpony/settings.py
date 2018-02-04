9d5cb0ca6681ef5a382b7f4a067be46301f321af
Iman van Assum <iman@vanassum.net>
2018-02-04 13:45:26 +0100

    Fixed typo in settings.py

--- intelpony/settings.py
+++ intelpony/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'utopiapo']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'utopiaponies.vanassum.net']
 
 
 # Application definition
